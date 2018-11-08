4674c5e5957bb67e354451f997ce79fc11af2a82
Steffengra <steffengrac@gmail.com>
2018-11-08 16:00:41 +0100

    Delete tx_filter3.m

--- tx_filter3.m
+++
@@ -1,8 +0,0 @@
-function s = tx_filter3(d, par_tx, switch_graph)
-%upsample
-for i = length(d):-1:1
-    d = [d(1:i);zeros(par_tx-1,1);d(i+1:end)];
-end
-
-filter = dsp.LowpassFilter('PassbandFrequency', 1/par_tx, 'PassbandRipple', 0.001, 'DesignForMinimumOrder', true);
-fvtool(filter);
